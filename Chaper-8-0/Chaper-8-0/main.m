a3416e9aae58bf02381c5416446170e3495dc8d7
ccyag <22377832@qq.com>
2019-11-02 10:29:59 +0800

    NSRange, CGPoint, CGSize, CGRect 表示范围和几何数据类型。

--- Chaper-8-0/Chaper-8-0/main.m
+++ Chaper-8-0/Chaper-8-0/main.m
@@ -12,6 +12,21 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        NSRange range;
+        range.length = 4;
+        range.location = 17;
+        NSRange range1 = {17, 4};
+        __unused NSRange range2 = NSMakeRange(17, 4);
+        NSLog(@"%lu %lu", (unsigned long)range1.location, (unsigned long)range1.length);
+        
+        //
+        CGPoint point = CGPointMake(0, 0);
+        CGSize size = CGSizeMake(100, 100);
+        CGRect rect;
+        rect.origin = point;
+        rect.size = size;
+        
+        NSLog(@"%.2f %.2f %.2f %.2f", rect.origin.x, rect.origin.y, rect.size.width, rect.size.height);
     }
     return 0;
 }
