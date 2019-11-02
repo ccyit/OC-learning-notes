ec26ac43d14ba552f7d1fe9e4074b3cf2d9487b5
ccyag <22377832@qq.com>
2019-11-02 11:55:38 +0800

    [NSNull null] 空值，可以插入字典和数组中表示空值。

--- Chapter-8-3/Chapter-8-3/main.m
+++ Chapter-8-3/Chapter-8-3/main.m
@@ -12,6 +12,19 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        NSRect rect = {10, 20, 30, 40};
+        NSValue *value = [NSValue valueWithBytes:&rect objCType:@encode(NSRect)];
+        NSMutableArray *array = [NSMutableArray arrayWithCapacity:2];
+        [array addObject:value];
+        
+        value = array[0];
+        
+        NSRect rect2;
+        [value getValue:&rect2];
+        NSLog(@"%.2f %.2f %.2f %.2f", rect2.origin.x, rect2.origin.y, rect2.size.width, rect2.size.height);
+        
+        [array addObject:[NSNull null]];
+        NSLog(@"%@", array);
     }
     return 0;
 }
