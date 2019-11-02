634ead4029b6a918ec99f2150be6a5088fde45c9
ccyag <22377832@qq.com>
2019-11-02 09:40:34 +0800

    修改公司名称并添加类前缀CYC

---
+++ Chapter-7-1/Chapter-7-1/CYCTest.h
@@ -0,0 +1,17 @@
+//
+//  CYCTest.h
+//  Chapter-7-1
+//
+//  Created by ccyag on 2/11/2019.
+//  Copyright © 2019 com.ccyag.test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CYCTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Chapter-7-1/Chapter-7-1/CYCTest.m
@@ -0,0 +1,13 @@
+//
+//  CYCTest.m
+//  Chapter-7-1
+//
+//  Created by ccyag on 2/11/2019.
+//  Copyright © 2019 com.ccyag.test. All rights reserved.
+//
+
+#import "CYCTest.h"
+
+@implementation CYCTest
+
+@end
