8bc0aa4128ee8e89be681ebf6e869bffc13a6da3
ccyag <22377832@qq.com>
2019-11-02 09:16:06 +0800

    深入理解Xcode ，version Xcode11。

---
+++ Chapter-7-1/Chapter-7-1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Chapter-7-1
+//
+//  Created by ccyag on 2/11/2019.
+//  Copyright © 2019 com.ccyag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
