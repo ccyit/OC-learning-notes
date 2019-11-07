680727342c7aec9c96b7063031e8dc67a47d77d4
ccyag <22377832@qq.com>
2019-11-07 18:32:08 +0800

    NSPropertyListSerialization

--- chapter17-1/chapter17-1/main.m
+++ chapter17-1/chapter17-1/main.m
@@ -17,6 +17,34 @@ int main(int argc, const char * argv[]) {
         NSDate *yesterday = [NSDate dateWithTimeIntervalSinceNow:-(24 * 60 * 60)];
         NSLog(@"yesterday is %@", yesterday);
         
+        NSDictionary *dic = @{@"FirstName": @"Chen",
+                              @"LastName": @"Chongyang"};
+        
+        NSData * data_binary = [NSPropertyListSerialization dataWithPropertyList:dic format:NSPropertyListBinaryFormat_v1_0 options:0 error:nil];
+        NSLog(@"data = %@", data_binary);
+        [data_binary writeToFile:@"/tmp/BinaryFormatData" atomically:YES];
+
+      
+        
+        
+        NSData *data_xml = [NSPropertyListSerialization dataWithPropertyList:dic format:NSPropertyListXMLFormat_v1_0 options:0 error:nil];
+        NSLog(@"data = %@", data_xml);
+        [data_xml writeToFile:@"/tmp/XMLFormatData" atomically:YES];
+
+        NSPropertyListFormat format;
+        
+        NSMutableDictionary *dic2 = [NSPropertyListSerialization propertyListWithData:data_binary options:NSPropertyListMutableContainersAndLeaves format:&format error:nil];
+        
+        NSLog(@"%ld", format);
+        NSLog(@"%@", dic2);
+        
+        NSMutableDictionary *dic3 = [NSPropertyListSerialization propertyListWithData:data_xml options:NSPropertyListMutableContainersAndLeaves format:&format error:nil];
+        
+        NSLog(@"%ld", format);
+        NSLog(@"%@", dic3);
+        
+        
+        
         
     }
     return 0;
