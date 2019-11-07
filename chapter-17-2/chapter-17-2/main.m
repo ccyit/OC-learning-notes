9afcbd260dde4171422ad1843cb00030ba2bcf14
ccyag <22377832@qq.com>
2019-11-07 18:37:03 +0800

    对象编码，序列化

---
+++ chapter-17-2/chapter-17-2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  chapter-17-2
+//
+//  Created by ccyag on 7/11/2019.
+//  Copyright © 2019 com.ccyag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
