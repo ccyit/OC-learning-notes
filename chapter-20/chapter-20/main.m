08b0290ddaa5b66f3cabcd4611773140a98a1640
ccyag <22377832@qq.com>
2019-11-10 15:57:00 +0800

    NSPredicate。

---
+++ chapter-20/chapter-20/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  chapter-20
+//
+//  Created by ccyag on 10/11/2019.
+//  Copyright © 2019 com.ccyag. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
